ee68546e5ced03f950f46fe5b3b5ac5788bb0c84
rameez.khalil <rameezkhalil23@gmail.com>
2023-11-09 03:04:22 -0800

    first commit for j2ee

---
+++ ServletsLearning/src/com/servlet/HelloWorldServlet.java
@@ -0,0 +1,56 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloWorldServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// setting the content type:
+		response.setContentType("text/html");
+
+		// getting the printwriter:
+		PrintWriter out = response.getWriter();
+
+		// Generating HTML:
+		out.println("<html><body>");
+		out.println("<h1>Hello Rameez<h1>");
+		out.println("Time on server is: " + new java.util.Date());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
